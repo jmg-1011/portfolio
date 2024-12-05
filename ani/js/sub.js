2bf23a67224f53f7e5eefca3f98a6442da74fad8
jmg-1011 <whalsrms77@gmail.com>
2024-12-05 20:58:10 +0900

    ani update

--- ani/js/sub.js
+++ ani/js/sub.js
@@ -1,45 +1,52 @@
-$(function(){
+$(function () {
 
     //플레시 효과 시작
-        const $images = $('.flash img');
-        let currentIndex = 0; 
-        function flashEffect() {
-            $images.hide();
-            $images.eq(currentIndex).fadeIn(500).delay(500).fadeOut(500);
-            currentIndex = (currentIndex + 1) % $images.length;
-            setTimeout(flashEffect, 1500); 
-        }
-        flashEffect(); 
+    const $images = $('.flash img');
+    let currentIndex = 0;
+    function flashEffect() {
+        $images.hide();
+        $images.eq(currentIndex).fadeIn(500).delay(500).fadeOut(500);
+        currentIndex = (currentIndex + 1) % $images.length;
+        setTimeout(flashEffect, 1500);
+    }
+    flashEffect();
     //플레시 효과 끝
 
 
 
     //한숨 쉬는 효과 시작
-        $('.person').click(function() {
-            $(this).toggleClass('on'); });
+    $('.person').click(function () {
+        $(this).toggleClass('on');
+    });
     //한숨 쉬는 효과 끝
 
     //f1레이싱카 설명 시작
-    $('.f1_car').click(function(){
+    $('.f1_car').click(function () {
         $('.f1_txtbox').fadeIn();
     });
-    $('.f1_txtbox button').click(function(e){
+    $('.f1_txtbox button').click(function (e) {
         e.stopPropagation();
         $('.f1_txtbox').fadeOut();
     });
 
-    $('.racing_car').click(function(){
+    $('.racing_car').click(function () {
         $('.racing_txtbox').fadeIn();
     });
-    $('.racing_car button').click(function(e){
+    $('.racing_car button').click(function (e) {
         e.stopPropagation();
         $('.racing_txtbox').fadeOut();
     });
-    
+
     //f1레이싱카 설명 끝
 
 
-    $('.lifgt .left,.lifgt .right').click(function(){
-        $(this).toggleClass('on')
+    //시상대 조명 시작
+
+    $('.lifgt .left, .lifgt .right').click(function () {
+        $('.lifgt .left, .lifgt .right').toggleClass('on');
     });
+
+
+    //시상대 조명 끝
+
 }); 
